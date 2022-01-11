307075ad49b092c9242165c344df12fbb1ecd66d
astrutz <--replace-all>
2022-01-11 10:21:57 +0100

    Add server configurations

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const createError = require('http-errors');
 const requestId = require('@ablegroup/requestid');
 
 const app = express();
-const PORT = 3003;
+const PORT = 3001;
+const BASE_PATH = '/webhooks';
 
 const rootRoute = require('./routes/root.js')();
 const testhookRoute = require('./routes/testhook.js')();
@@ -17,14 +18,14 @@ app.use(logger('[ctx@49610 rid=":requestId"][http@49610 method=":method" url=":u
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/', rootRoute);
-app.use('/test', testhookRoute);
+app.use(`${BASE_PATH}/`, rootRoute);
+app.use(`${BASE_PATH}/test`, testhookRoute);
+
+app.listen(PORT, () => {
+  console.log(`Application is running on port ${PORT}.`);
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
-});
-
-app.listen(PORT, () => {
-  console.log(`Application is running on port ${PORT}.`);
 });
